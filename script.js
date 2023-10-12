06fb7dad574acb04fb5c1d1453282614b25b0d40
MansiRK <19302009mansi@viva-technology.org>
2023-10-12 19:03:20 +0530

    added comments

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
+// Getting the filter container and image items
 const filterImgContainer = document.querySelector(".image-filter");
 const imageItems = document.querySelectorAll(".image-item");
 
+// Adding click event listener to the filter container
 filterImgContainer.addEventListener("click", (event) => {
+  // Check if the clicked element has the 'filter-tabs' class
   if (event.target.classList.contains("filter-tabs")) {
     const filterImg = event.target.getAttribute("data-filter");
+
     filterImages(filterImg);
     updateActiveFilter(event.target);
   }
 });
 
+// Function to filter images based on the selected filter
 function filterImages(filter) {
   imageItems.forEach((item) => {
     if (filter === "all" || item.classList.contains(filter)) {
@@ -21,6 +26,7 @@ function filterImages(filter) {
   });
 }
 
+// Function to update the active filter style
 function updateActiveFilter(activeFilter) {
   const activeFilterClass = "active";
   const currentActiveFilter = filterImgContainer.querySelector(
