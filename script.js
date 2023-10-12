6e662fb0f06ffc2cafbab9fb68c4f2959d0f5c6b
MansiRK <19302009mansi@viva-technology.org>
2023-10-12 18:58:04 +0530

    added styling

--- script.js
+++ script.js
@@ -1,22 +1,35 @@
-const filterImgContainer = document.querySelector(".image-filter"),
-  imageItems = document.querySelectorAll(".image-item");
+const filterImgContainer = document.querySelector(".image-filter");
+const imageItems = document.querySelectorAll(".image-item");
 
 filterImgContainer.addEventListener("click", (event) => {
-  if (event.target.classList.contains("filter-image")) {
-    // deactivate existing active 'filter-item'
-    filterImgContainer.querySelector(".active").classList.remove("active");
-
-    // activate new 'filter-item'
-    event.target.classList.add("active");
+  if (event.target.classList.contains("filter-tabs")) {
     const filterImg = event.target.getAttribute("data-filter");
-    imageItems.forEach((item) => {
-      if (item.classList.contains(filterImg) || filterImg === "all") {
-        item.classList.remove("hide");
-        item.classList.add("show");
-      } else {
-        item.classList.remove("show");
-        item.classList.add("hide");
-      }
-    });
+    filterImages(filterImg);
+    updateActiveFilter(event.target);
   }
 });
+
+function filterImages(filter) {
+  imageItems.forEach((item) => {
+    if (filter === "all" || item.classList.contains(filter)) {
+      item.classList.remove("hide");
+      item.classList.add("show");
+    } else {
+      item.classList.remove("show");
+      item.classList.add("hide");
+    }
+  });
+}
+
+function updateActiveFilter(activeFilter) {
+  const activeFilterClass = "active";
+  const currentActiveFilter = filterImgContainer.querySelector(
+    `.${activeFilterClass}`
+  );
+
+  if (currentActiveFilter) {
+    currentActiveFilter.classList.remove(activeFilterClass);
+  }
+
+  activeFilter.classList.add(activeFilterClass);
+}
